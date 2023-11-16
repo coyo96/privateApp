e0bf0fba870a3893aa188d5340247776b70b374f
saker132 <saker132@gmail.com>
2023-11-16 12:41:04 -0600

    Added test vue frontend for auth testing

---
+++ frontend-vue/src/main.ts
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(
+    createAuth0({
+        domain: 'dev-sethaker.us.auth0.com',
+        clientId: "Ud60sEpQ4lzv5RVj3TZQgTrmv9A5GCID",
+        authorizationParams: {
+            redirect_uri: window.location.origin 
+        }
+    })
+)
+
+app.mount('#app')
---
+++ frontend-vue/src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useAuth0 } from "@auth0/auth0-vue";
+import LoginButton from '@/components/LoginButton.vue';
+import LogoutButton from '@/components/LogoutButton.vue';
+
+const { user, isAuthenticated, isLoading } = useAuth0(); 
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <LoginButton></LoginButton>
+    <LogoutButton/>
+    <div v-if="!isLoading && isAuthenticated">
+        <code>{{ user }}</code>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
