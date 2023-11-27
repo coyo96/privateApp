50e3e39a330efa98bff9dc6637aeeb2b4be3b7c3
saker132 <saker132@gmail.com>
2023-11-26 21:04:58 -0600

    Changed a little thing

--- frontend-vue/src/views/AboutView.vue
+++ frontend-vue/src/views/AboutView.vue
@@ -3,9 +3,13 @@ import { useAuth0 } from "@auth0/auth0-vue";
 import LoginButton from '@/components/LoginButton.vue';
 import LogoutButton from '@/components/LogoutButton.vue';
 import TestBackendCall from "@/components/TestBackendCall.vue";
-
+import { useCounterStore } from "@/stores/counter";
 const { user, isAuthenticated, isLoading } = useAuth0(); 
+
+const counterStore  = useCounterStore();
+
 </script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
@@ -13,8 +17,13 @@ const { user, isAuthenticated, isLoading } = useAuth0();
     <LogoutButton/>
     <div v-if="!isLoading && isAuthenticated">
         <code>{{ user }}</code>
+        <img>
     </div>
     <TestBackendCall />
+
+    <h1>{{ counterStore.count }}</h1>
+    <h1>{{ counterStore.doubleCount }}</h1>
+    <button @click="counterStore.increment()">Increment</button>
   </div>
 </template>
 
