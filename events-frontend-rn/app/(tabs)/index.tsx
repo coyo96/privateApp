99c2ec5d7959086c12a7a7868d1b9298b47f39bf
saker132 <saker132@gmail.com>
2023-12-05 19:31:37 -0600

    Added auth0

--- events-frontend-rn/app/(tabs)/index.tsx
+++ events-frontend-rn/app/(tabs)/index.tsx
@@ -1,14 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { useAuth0 } from 'react-native-auth0'
 
 export default function TabOneScreen() {
+  const { authorize, clearSession, user, error, isLoading} = useAuth0();
+  const onLogin = async () => {
+    try {
+      await authorize();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onLogout = async () => {
+    try {
+      await clearSession();
+    } catch (e) {
+      console.log('Log out cancelled');
+    }
+  };
+
+  if (isLoading) {
+    return <View style={styles.container}><Text>Loading</Text></View>;
+  }
+
+  const loggedIn = user !== undefined && user !== null;
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      {loggedIn && <Text>You are logged in as {user.name}</Text>}
+      {!loggedIn && <Text>You are not logged in</Text>}
+      {error && <Text>{error.message}</Text>}
+
+      <Button
+        onPress={loggedIn ? onLogout : onLogin}
+        title={loggedIn ? 'Log Out' : 'Log In'}
+      />
     </View>
   );
 }
